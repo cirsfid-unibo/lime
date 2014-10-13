234f69cb1786aadd113582c4e2697a9bc543280c
Matteo Nardi <m.nardi@unibo.it>
2014-10-13 11:51:52 +0200

    Missing files in previous commit

---
+++ ext/build/examples/kitchensink/app/view/charts/area/Basic.js
@@ -0,0 +1,155 @@
+/**
+ * A basic area chart is similar to the line chart, except the area between axis and line
+ * is filled with colors to emphasize quantity.
+ */
+Ext.define('KitchenSink.view.charts.area.Basic', {
+    extend: 'Ext.Panel',
+    xtype: 'area-basic',
+
+    // <example>
+    // Content between example tags is omitted from code preview.
+    bodyStyle: 'background: transparent !important',
+
+    layout: {
+        type: 'vbox',
+        pack: 'center'
+    },
+    // </example>
+
+    width: 650,
+
+    initComponent: function() {
+        var me = this;
+
+        me.myDataStore = Ext.create('Ext.data.JsonStore', {
+            fields: ['month', 'data1' ],
+            data: [
+                { month: 'Jan', data1: 20 },
+                { month: 'Feb', data1: 20 },
+                { month: 'Mar', data1: 19 },
+                { month: 'Apr', data1: 18 },
+                { month: 'May', data1: 18 },
+                { month: 'Jun', data1: 17 },
+                { month: 'Jul', data1: 16 },
+                { month: 'Aug', data1: 16 },
+                { month: 'Sep', data1: 16 },
+                { month: 'Oct', data1: 16 },
+                { month: 'Nov', data1: 15 },
+                { month: 'Dec', data1: 15 }
+            ]
+        });
+        //<example>
+        me.tbar = [
+            '->',
+            {
+                text: 'Preview',
+                handler: function() {
+                    me.down('cartesian').preview();
+                }
+            }
+        ];
+        //</example>
+
+        me.items = [{
+            xtype: 'cartesian',
+            width: '100%',
+            height: 500,
+            store: this.myDataStore,
+            insetPadding: 40,
+            sprites: [{
+                type: 'text',
+                text: 'Area Charts - Basic Area',
+                fontSize: 22,
+                width: 100,
+                height: 30,
+                x: 40, // the sprite x position
+                y: 20  // the sprite y position
+            }, {
+                type: 'text',
+                text: 'Data: Browser Stats 2012 - Internet Explorer',
+                fontSize: 10,
+                x: 12,
+                y: 480
+            }, {
+                type: 'text',
+                text: 'Source: http://www.w3schools.com/',
+                fontSize: 10,
+                x: 12,
+                y: 495
+            }],
+            axes: [{
+                type: 'numeric',
+                position: 'left',
+                grid: true,
+                fields: ['data1'],
+                renderer: function (v, layoutContext) {
+                    // Custom renderer overrides the native axis label renderer.
+                    // Since we don't want to do anything fancy with the value
+                    // ourselves except appending a '%' sign, but at the same time
+                    // don't want to loose the formatting done by the native renderer,
+                    // we let the native renderer process the value first.
+                    return layoutContext.renderer(v) + '%';
+                },
+                minimum: 0,
+                maximum: 24
+            }, {
+                type: 'category',
+                position: 'bottom',
+                grid: true,
+                fields: ['month'],
+                label: {
+                    rotate: {
+                        degrees: -45
+                    }
+                }
+            }],
+            series: [{
+                type: 'area',
+                axis: 'left',
+                xField: 'month',
+                yField: 'data1',
+                style: {
+                    opacity: 0.80
+                },
+                marker: {
+                    opacity: 0,
+                    scaling: 0.01,
+                    fx: {
+                        duration: 200,
+                        easing: 'easeOut'
+                    }
+                },
+                highlightCfg: {
+                    opacity: 1,
+                    scaling: 1.5
+                },
+                tooltip: {
+                    trackMouse: true,
+                    style: 'background: #fff',
+                    renderer: function(storeItem, item) {
+                        this.setHtml(storeItem.get('month') + ': ' + storeItem.get('data1') + ' %');
+                    }
+                }
+            }]
+        //<example>
+        }, {
+            style: 'margin-top: 10px;',
+            xtype: 'gridpanel',
+            columns : {
+                defaults: {
+                    sortable: false,
+                    menuDisabled: true
+                },
+                items: [
+                    { text: 'Month', dataIndex: 'month' },
+                    { text: 'IE', dataIndex: 'data1', renderer: function(v) { return v + '%'; } }
+                ]
+            },
+            store: this.myDataStore,
+            width: '100%'
+        //</example>
+        }];
+
+        this.callParent();
+    }
+});
