27bbadf17a54b7edad66d0a08de6ec97b9a5925f
Matteo Nardi <m.nardi@unibo.it>
2014-10-16 09:36:22 +0200

    Remove build from Git.

--- build/temp/production/LIME/sass/config.rb
+++
@@ -1,3 +0,0 @@
-require '..\..\..\..\..\ext\packages\ext-theme-base\sass\utils.rb'
-require '..\..\..\..\..\sass\config.rb'
-cache_path = 'C:\Users\octavian.bujor2\Documents\EasyPHP-5.3.9\www\lime5-git/build/.sass-cache'
--- build/temp/production/LIME/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require C:\Users\octavian.bujor2\Documents\EasyPHP-5.3.9\www\lime5-git\app.js
