6f1966affec8c37191e4020ab5cec1d6468d852b
Matteo Nardi <m.nardi@unibo.it>
2015-06-18 11:48:22 +0200

    Add generic node server path

--- packages/lime-core/src/store/OpenFile.js
+++ packages/lime-core/src/store/OpenFile.js
@@ -101,7 +101,7 @@ Ext.define('LIME.store.OpenFile', {
                 var path = store.requestNode;
                 if (path[path.length -1] != '/') path += '/';
                 var proxy = this.getProxy();
-                proxy.url = Server.nodeServer + '/Documents' + path;
+                proxy.url = Server.nodeServer + '/documentsdb/Documents' + path;
                 proxy.headers = {
                     Authorization: 'Basic ' + Ext.util.Base64.encode(User.username + ':' + User.password)
                 };
