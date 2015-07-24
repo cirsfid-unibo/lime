1fded2d6f3b09719d189d23b98920bdf6d8224a9
Matteo Nardi <m.nardi@unibo.it>
2015-07-24 14:22:39 +0200

    Fix bugs

--- packages/default-autoimport-open/src/controller/CustomOpenButton.js
+++ packages/default-autoimport-open/src/controller/CustomOpenButton.js
@@ -1,40 +1,40 @@
 /*
  * Copyright (c) 2014 - Copyright holders CIRSFID and Department of
  * Computer Science and Engineering of the University of Bologna
- * 
- * Authors: 
+ *
+ * Authors:
  * Monica Palmirani – CIRSFID of the University of Bologna
  * Fabio Vitali – Department of Computer Science and Engineering of the University of Bologna
  * Luca Cervone – CIRSFID of the University of Bologna
- * 
+ *
  * Permission is hereby granted to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The Software can be used by anyone for purposes without commercial gain,
  * including scientific, individual, and charity purposes. If it is used
  * for purposes having commercial gains, an agreement with the copyright
  * holders is required. The above copyright notice and this permission
  * notice shall be included in all copies or substantial portions of the
  * Software.
- * 
+ *
  * Except as contained in this notice, the name(s) of the above copyright
  * holders and authors shall not be used in advertising or otherwise to
  * promote the sale, use or other dealings in this Software without prior
  * written authorization.
- * 
+ *
  * The end-user documentation included with the redistribution, if any,
  * must include the following acknowledgment: "This product includes
  * software developed by University of Bologna (CIRSFID and Department of
- * Computer Science and Engineering) and its authors (Monica Palmirani, 
+ * Computer Science and Engineering) and its authors (Monica Palmirani,
  * Fabio Vitali, Luca Cervone)", in the same place and form as other
  * third-party acknowledgments. Alternatively, this acknowledgment may
  * appear in the software itself, in the same form and location as other
  * such third-party acknowledgments.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -50,19 +50,19 @@
 // Pdf file: ???
 Ext.define('DefaultAutoimportOpen.controller.CustomOpenButton', {
     extend: 'Ext.app.Controller',
-    
+
     refs: [
-        { ref: 'appViewport', selector: 'appViewport' }, 
-        { ref: 'pdf',         selector: 'pdf' }, 
+        { ref: 'appViewport', selector: 'appViewport' },
+        { ref: 'pdf',         selector: 'pdf' },
         { ref: 'exportMenu',  selector: 'openDocumentButton' },
         { ref: 'openButton',  selector: 'openDocumentButton' }
     ],
-    
+
     init: function () {
         this.replaceOpenMenu();
     },
 
-    // Make sure the open button cam open NIR, DOC and PDF files. 
+    // Make sure the open button cam open NIR, DOC and PDF files.
     replaceOpenMenu: function () {
         this.application.fireEvent("addMenuItem", this, {
             menu: "fileMenuButton",
@@ -84,7 +84,7 @@ Ext.define('DefaultAutoimportOpen.controller.CustomOpenButton', {
                 var extension = path.substring(path.length - 3);
                 if(extension == 'pdf')
                     me.onPdfSelected(path);
-                else if(extension == 'doc')
+                else if(extension == 'doc' || path.substring(path.length - 4) == 'docx')
                     me.onDocSelected(path);
                 else if (extension == 'xml') {
                     Server.getDocument(path, function (content) {
@@ -109,7 +109,7 @@ Ext.define('DefaultAutoimportOpen.controller.CustomOpenButton', {
         this.onBinaryDocumentSelected(path);
     },
 
-    onBinaryDocumentSelected: function (path) {        
+    onBinaryDocumentSelected: function (path) {
         var me = this;
         Server.fileToHtml(path, function (html, lang) {
             console.log('html', html, lang);
@@ -129,7 +129,7 @@ Ext.define('DefaultAutoimportOpen.controller.CustomOpenButton', {
 
     onNirSelected: function (path, content) {
         console.log('onNirSelected', path);
-        var me = this; 
+        var me = this;
         //NirUtils.confirmAknTranslation(function () {
             NirUtils.nirToHtml(content, function(html) {
             // Load the resulting Htmltoso document
