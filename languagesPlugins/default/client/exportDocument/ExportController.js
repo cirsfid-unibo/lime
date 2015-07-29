f3d597e9fc917f146263a4aca5a179b6318fc736
Matteo Nardi <commits@matteonardi.org>
2015-07-29 15:44:30 +0200

    Add FileSaver lib. Use it to export xml

--- languagesPlugins/default/client/exportDocument/ExportController.js
+++ languagesPlugins/default/client/exportDocument/ExportController.js
@@ -1,40 +1,40 @@
 /*
  * Copyright (c) 2014 - Copyright holders CIRSFID and Department of
  * Computer Science and Engineering of the University of Bologna
- * 
- * Authors: 
+ *
+ * Authors:
  * Monica Palmirani – CIRSFID of the University of Bologna
  * Fabio Vitali – Department of Computer Science and Engineering of the University of Bologna
  * Luca Cervone – CIRSFID of the University of Bologna
- * 
+ *
  * Permission is hereby granted to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The Software can be used by anyone for purposes without commercial gain,
  * including scientific, individual, and charity purposes. If it is used
  * for purposes having commercial gains, an agreement with the copyright
  * holders is required. The above copyright notice and this permission
  * notice shall be included in all copies or substantial portions of the
  * Software.
- * 
+ *
  * Except as contained in this notice, the name(s) of the above copyright
  * holders and authors shall not be used in advertising or otherwise to
  * promote the sale, use or other dealings in this Software without prior
  * written authorization.
- * 
+ *
  * The end-user documentation included with the redistribution, if any,
  * must include the following acknowledgment: "This product includes
  * software developed by University of Bologna (CIRSFID and Department of
- * Computer Science and Engineering) and its authors (Monica Palmirani, 
+ * Computer Science and Engineering) and its authors (Monica Palmirani,
  * Fabio Vitali, Luca Cervone)", in the same place and form as other
  * third-party acknowledgments. Alternatively, this acknowledgment may
  * appear in the software itself, in the same form and location as other
  * such third-party acknowledgments.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -51,16 +51,16 @@ Ext.define('LIME.controller.ExportController', {
         ref : 'downloadManager',
         selector : 'downloadManager'
     }],
-    
+
     config : {
         pluginName : "exportDocument"
     },
-    
+
     initMenu : function() {
         var me = this;
         me.addExportItem();
     },
-    
+
     addExportItem: function() {
         var me = this;
         menu = {
@@ -98,13 +98,14 @@ Ext.define('LIME.controller.ExportController', {
             menu : "fileMenuButton"
         }, menu);
     },
-    
+
     /**
      * This function exports the document using the download manager
      * @param {String} url The url of download service
      * @param {Object} params Params to pass to the download service
      */
     exportDocument: function(url, params) {
+        console.info('exportDocument', url, params);
         var downloadManager = this.getDownloadManager();
         // Set a callback function to translateContent
         this.application.fireEvent(Statics.eventsNames.translateRequest, function(xml) {
@@ -112,20 +113,21 @@ Ext.define('LIME.controller.ExportController', {
             downloadManager.fireEvent(downloadManager.eventActivate, url, parameters);
         }, {complete: true});
     },
-    
+
     onInitPlugin: function() {
         var me = this;
         me.initMenu();
     },
-            
+
     init : function() {
         var me = this;
 
         this.control({
             'menu [name=exportXmlButton]': {
                 click: function() {
-                    me.exportDocument(Utilities.getAjaxUrl(), {
-                        requestedService: Statics.services.xmlExport
+                    Server.getDocument(DocProperties.documentInfo.docId, function (xml) {
+                        var blob = new Blob([xml], {type: "text/xml;charset=utf-8"});
+                        saveAs(blob, "document.xml");
                     });
                 }
             },
