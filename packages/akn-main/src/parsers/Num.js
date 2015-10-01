f120865c8e1afa07910313a79ddd3d4a772d0de7
Octavian Bujor <octavian.bujor@unibo.it>
2015-10-01 13:37:49 +0200

    Akn num parser bugfix

--- packages/akn-main/src/parsers/Num.js
+++ packages/akn-main/src/parsers/Num.js
@@ -53,7 +53,7 @@
 Ext.define('AknMain.parsers.Num', {
     singleton: true,
     
-    numReg: new RegExp(/(?:[\u00C0-\u1FFF\u2C00-\uD7FF\w]+\W*\s*)*((\w+[\.)\s]*)+)/),
+    numReg: new RegExp(/(?:[\u00C0-\u1FFF\u2C00-\uD7FF\w]+\W*\s+)*((\w+[\.)\s]*)+)/),
     romanReg: new RegExp(/M{0,4}(?:CM|CD|D?C{0,3})(?:XC|XL|L?X{0,3})(?:IX|IV|V?I{0,3})/),
 
     normalize: function(str) {
