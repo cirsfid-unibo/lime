37abd976b1cd426718d1d6d2762f6c20a99a05f1
Octavian Bujor <octavian.bujor@unibo.it>
2017-02-23 12:22:24 +0100

    Moved akoma3.0 to its repo lime-language-akn

--- languagesPlugins/akoma3.0/scripts/sass.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sass --style compressed --sourcemap=none styles/content.scss interface/default/content.css
-sass --style compressed --sourcemap=none styles/akn_it.scss interface/default/it/content.css
-sass --style compressed --sourcemap=none styles/akn_es.scss interface/default/es/content.css
