0ba40c9674c8507e94fa32623b4ff78cca671494
Carlo Stomeo <stomcarlo@gmail.com>
2017-11-28 12:37:15 +0100

    overridden core getNodeExtraInfo in akn main package. fixed #57

--- packages/lime-core/src/DomUtils.js
+++ packages/lime-core/src/DomUtils.js
@@ -341,38 +341,7 @@ Ext.define('LIME.DomUtils', {
      * @returns {String}
      */
     getNodeExtraInfo : function(node, type, limit) {
-        var info = '';
-        if (node && node.getAttribute("class") && node.getAttribute("class").indexOf(type) != -1) {
-            var infoLength = limit || Statics.extraInfoLimit;
-            var wrapper = Ext.fly(node);
-            var where = ["num", "heading", "subheading"];
-            var infoNode;
-            for (var i = 0; i < where.length; i++) {
-                var contentEl = wrapper.child(".content");
-                if (contentEl) {
-                    wrapper = contentEl;
-                }
-                var chNode = wrapper.down("." + where[i]);
-                if (chNode) {
-                    var hcontainer = chNode.parent(".hcontainer");
-                    if (hcontainer && hcontainer.dom == node) {
-                        infoNode = chNode;
-                        break;
-                    }
-                }
-            }
-            if (infoNode) {
-                for(var i = 0; i < infoNode.dom.childNodes.length; i++) {
-                    if(infoNode.dom.childNodes[i].nodeType == this.nodeType.TEXT) {
-                        info+= infoNode.dom.childNodes[i].textContent;
-                    }
-                }
-            }
-            if (info.length > infoLength) {
-                info = info.substr(0, infoLength) + "...";
-            }
-        }
-        return info;
+        return '';
     },
     /**
      * This function retrieves the first marked ascendant
