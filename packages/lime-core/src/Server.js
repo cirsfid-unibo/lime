1887d6e73f9d75822a642042e4f2b42473cbefd1
Carlo Stomeo <stomcarlo@gmail.com>
2017-12-12 10:32:21 +0100

    added OPTIONS method usage to check if a file exists, Fixed #59

--- packages/lime-core/src/Server.js
+++ packages/lime-core/src/Server.js
@@ -92,7 +92,7 @@ Ext.define('LIME.Server', {
         }
 
         var possiblePaths = [];
-        
+
         if (Ext.manifest['env'] === 'development') {
             possiblePaths.push({
                 name: 'dev',
@@ -122,20 +122,20 @@ Ext.define('LIME.Server', {
     // - content: auto-read response ...
     // Check (Server-side) which file exist and return them (If 'content' param is set to true)
     // Example reqUrls: [{"name":"patterns","url":"config/Patterns.json"},
-    // TODO: using OPTIONS method when content is set to false
     filterUrls: function (reqUrls, content, success, failure, scope) {
         var me = this;
         var newUrls = [];
         var makeRequest = function(urlObj, cb) {
             me.request({
                 url: urlObj.url,
+                method: (content) ? 'GET' : 'OPTIONS',
                 success: function (result) {
                     var content = result.responseText;
                     if (content) {
                         var json = Ext.decode(content, true);
                         urlObj.content = json || content;
-                        newUrls.push(urlObj);
                     }
+                    newUrls.push(urlObj); //I don't check result.status because we already are in success function
                 },
                 callback: cb
             });
