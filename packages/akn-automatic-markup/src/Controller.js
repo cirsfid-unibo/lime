ecaa5c21c30370c39fcece84919cf5ed2d203f53
Octavian Bujor <octavian.bujor@unibo.it>
2017-12-12 13:09:21 +0100

    Automatic markup fix wrap body parts

--- packages/akn-automatic-markup/src/Controller.js
+++ packages/akn-automatic-markup/src/Controller.js
@@ -1391,13 +1391,13 @@ Ext.define('AknAutomaticMarkup.Controller', {
             if ( !textNodesObj.length ) return;
             var textNodeObj = chooseTextObj(textNodesObj, numVal);
             if (!textNodeObj) return;
-            var firstNode = textNodeObj[0].node;
+            var numToMark = me.newTextNodeToSpans(textNodeObj);
+            var firstNode = numToMark[0];
             var partNode = (firstNode.parentNode == node) ? firstNode: firstNode.parentNode;
             var newWrapper = me.wrapPartNode(partNode, node);
             element.wrapper = newWrapper;
             nodesToMark.push(newWrapper);
-            numsToMark = Ext.Array.push(numsToMark, me.newTextNodeToSpans(textNodeObj));
-
+            numsToMark = Ext.Array.push(numsToMark, numToMark);
             if ( headingVal ) {
                 var text = DomUtils.smartFindTextNodes(headingVal, node);
                 if ( text.length ) {
