68091e742a549bad4c4eb570dd0c0a8df93d2683
Octavian Bujor <octavian.bujor@unibo.it>
2018-05-21 16:46:06 +0200

    New document window improved customizability

--- packages/lime-core/src/view/modal/NewDocument.js
+++ packages/lime-core/src/view/modal/NewDocument.js
@@ -1,40 +1,40 @@
 /*
  * Copyright (c) 2014 - Copyright holders CIRSFID and Department of
  * Computer Science and Engineering of the University of Bologna
- * 
- * Authors: 
+ *
+ * Authors:
  * Monica Palmirani – CIRSFID of the University of Bologna
  * Fabio Vitali – Department of Computer Science and Engineering of the University of Bologna
  * Luca Cervone – CIRSFID of the University of Bologna
- * 
+ *
  * Permission is hereby granted to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The Software can be used by anyone for purposes without commercial gain,
  * including scientific, individual, and charity purposes. If it is used
  * for purposes having commercial gains, an agreement with the copyright
  * holders is required. The above copyright notice and this permission
  * notice shall be included in all copies or substantial portions of the
  * Software.
- * 
+ *
  * Except as contained in this notice, the name(s) of the above copyright
  * holders and authors shall not be used in advertising or otherwise to
  * promote the sale, use or other dealings in this Software without prior
  * written authorization.
- * 
+ *
  * The end-user documentation included with the redistribution, if any,
  * must include the following acknowledgment: "This product includes
  * software developed by University of Bologna (CIRSFID and Department of
- * Computer Science and Engineering) and its authors (Monica Palmirani, 
+ * Computer Science and Engineering) and its authors (Monica Palmirani,
  * Fabio Vitali, Luca Cervone)", in the same place and form as other
  * third-party acknowledgments. Alternatively, this acknowledgment may
  * appear in the software itself, in the same form and location as other
  * such third-party acknowledgments.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -57,7 +57,7 @@ Ext.define('LIME.view.modal.NewDocument', {
     alias : 'widget.newDocument',
 
     layout : 'auto',
-    
+
     draggable : true,
 
     border : false,
@@ -65,7 +65,7 @@ Ext.define('LIME.view.modal.NewDocument', {
     modal : true,
 
     width : 200,
-    
+
     /**
      * Return the data set in the view
      * @return {Object} An object containing the key-value pairs in the form
@@ -81,7 +81,7 @@ Ext.define('LIME.view.modal.NewDocument', {
         this.title = Locale.strings.newDocument,
         this.items = [{
             // Form to type username and password (including buttons)
-    
+
             xtype : 'form',
             frame : true,
             padding : '10px',
@@ -89,29 +89,17 @@ Ext.define('LIME.view.modal.NewDocument', {
             defaults : {
                 anchor : '100%'
             },
-    
+
             // The fields
             defaultType : 'textfield',
-            items : [{
-                xtype : 'docMarkingLanguageSelector',
-                cls: 'syncType'
-            }, {
-                xtype : 'docTypeSelector',
-                cls: 'syncLocale',
-                hidden: true
-            }, {
-                xtype : 'docLocaleSelector',
-                hidden: true
-            }, {
-                xtype : 'docLangSelector'
-            }],
-    
+            items : this.getInputFields(),
+
             // The buttons
             dockedItems : [{
                 xtype : 'toolbar',
                 dock : 'bottom',
                 ui : 'footer',
-    
+
                 items : ['->', {
                     xtype : 'button',
                     minWidth : 100,
@@ -119,7 +107,23 @@ Ext.define('LIME.view.modal.NewDocument', {
                 }]
             }]
         }];
-        
+
         this.callParent(arguments);
+    },
+
+    getInputFields: function() {
+        return [{
+            xtype : 'docMarkingLanguageSelector',
+            cls: 'syncType'
+        }, {
+            xtype : 'docTypeSelector',
+            cls: 'syncLocale',
+            hidden: true
+        }, {
+            xtype : 'docLocaleSelector',
+            hidden: true
+        }, {
+            xtype : 'docLangSelector'
+        }];
     }
 });
